07d3ad0c302fed11747384289da2970be51ef4b3
Jason Santana <jsantana006@gmail.com>
2019-09-17 17:39:54 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,18 @@
 class Person 
+  #sets persons name
   def name=(person_name)
     @name = person_name
   end
-  
+  #gets persons name
   def name 
     @name
+  end
+  #sets persons job
+  def jobs=(persons_job)
+    @jobs
+  end
+  #gets persons job
+  def jobs
+    @jobs
   end
 end
